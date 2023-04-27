36a827c21bf4ae64e5f29cea8536482a28a2e570
Kubilay Akdemir <kubidik27@gmail.com>
2023-04-27 13:09:30 +0300

    edit Landing VC for segues

--- QShopping/LandingViewController.swift
+++ QShopping/LandingViewController.swift
@@ -8,18 +8,23 @@
 import UIKit
 
 class LandingViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
     
     
     @IBAction func authButtonPressed(_ sender: UIButton) {
+        if sender.tag == 0 {
+            performSegue(withIdentifier: K.segues.landingToLogin, sender: nil)
+        } else if sender.tag == 1 {
+            performSegue(withIdentifier: K.segues.landingToRegister, sender: nil)
+        }
     }
     
     @IBAction func testerRegisterPressed(_ sender: UIButton) {
-        
+        performSegue(withIdentifier: K.segues.landingToTester, sender: nil)
     }
     
 }
