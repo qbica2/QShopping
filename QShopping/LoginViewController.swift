255ff46b79268e8dab84c88ab5f5c57ca3b34c9f
Kubilay Akdemir <kubidik27@gmail.com>
2023-04-27 14:35:20 +0300

    define IBOutlets and IBActions for login VC

--- QShopping/LoginViewController.swift
+++ QShopping/LoginViewController.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameView: UIView!
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameErrorLabel: UILabel!
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func loginWithTestPressed(_ sender: Any) {
+    }
+    
 }
