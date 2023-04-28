5239e7f9e548ff8a88bfbaeb391dd6a65a9562b9
Kubilay Akdemir <kubidik27@gmail.com>
2023-04-28 14:31:50 +0300

    design UserCell

--- QShopping/Views/CustomCells/UserCell.swift
+++ QShopping/Views/CustomCells/UserCell.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class UserCell: UITableViewCell {
-
+    
+    
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+        
+    @IBOutlet weak var loginLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        loginLabel.layer.cornerRadius = 12
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
