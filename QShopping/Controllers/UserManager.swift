1e923dc296b52df92b8f37b5f9e1c9f26710c650
Kubilay Akdemir <kubidik27@gmail.com>
2023-04-28 14:23:00 +0300

    create UserManager

---
+++ QShopping/Controllers/UserManager.swift
@@ -0,0 +1,41 @@
+//
+//  UserManager.swift
+//  QShopping
+//
+//  Created by Mehmet Kubilay Akdemir on 27.04.2023.
+//
+
+import Foundation
+
+class UserManager {
+    
+    let baseUrl = "https://fakestoreapi.com/users"
+    var users: [User] = []
+    
+    func getUsers(completion: @escaping (Error?) -> Void) {
+        let url = URL(string: baseUrl)
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url!) { data, response, error in
+            if let error = error {
+                completion(error)
+            } else if let data = data {
+                let decoder = JSONDecoder()
+                do {
+                    let decodedData = try decoder.decode([UserData].self, from: data)
+                    
+                    for userData in decodedData {
+                        let user = User(email: userData.email, username: userData.username, password: userData.password, firstname: userData.name.firstname, lastname: userData.name.lastname)
+                        self.users.append(user)
+                    }
+                    
+                    completion(nil)
+                } catch {
+                    completion(error)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+}
+
---
+++ QShopping/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  QShopping
+//
+//  Created by Mehmet Kubilay Akdemir on 27.04.2023.
+//
+
+import Foundation
