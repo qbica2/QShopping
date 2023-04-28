9e90b3bac29289021aa830ca5d57189d6a700982
Kubilay Akdemir <kubidik27@gmail.com>
2023-04-28 16:23:32 +0300

    edit TesterRegister Vc for didSelectRow method

--- QShopping/Views/TesterRegisterViewController.swift
+++ QShopping/Views/TesterRegisterViewController.swift
@@ -15,6 +15,7 @@ class TesterRegisterViewController: UITableViewController {
         super.viewDidLoad()
         tableView.rowHeight = 100
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         userManager.getUsers { error in
             if error == nil {
@@ -34,12 +35,29 @@ class TesterRegisterViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cells.testerCell, for: indexPath) as! UserCell
         let user = userManager.users[indexPath.row]
+        
         cell.emailLabel.text = user.email
         cell.passwordLabel.text = user.password
         cell.userImage.image = UIImage(systemName: "person.circle")
         cell.usernameLabel.text = user.username
         cell.loginLabel.text = "Login as \(user.firstname)"
+        
         return cell
     }
-
+    //MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: K.segues.testerToLogin, sender: self)
+    }
+    
+    //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == K.segues.testerToLogin {
+            let loginVC = segue.destination as! LoginViewController
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let user = userManager.users[indexPath.row]
+                loginVC.selectedTester = user
+            }
+        }
+    }
 }
