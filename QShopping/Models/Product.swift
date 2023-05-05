f65e1f9702d1b7329b99d0e0b2896d3ea90c4abc
Kubilay Akdemir <kubidik27@gmail.com>
2023-05-05 14:32:58 +0300

    create product model

---
+++ QShopping/Models/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  QShopping
+//
+//  Created by Mehmet Kubilay Akdemir on 5.05.2023.
+//
+
+import Foundation
+
+struct Product {
+    let id: Int
+    let title: String
+    let price: Double
+    let description: String
+    let category: String
+    let imageURL: String
+    let rate: Double
+    let reviews: Int
+}
