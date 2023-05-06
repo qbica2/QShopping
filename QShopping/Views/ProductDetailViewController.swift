89ccf46562206f37dfe968d70872d813388fba40
Kubilay Akdemir <kubidik27@gmail.com>
2023-05-06 16:29:17 +0300

    use GettingProductDetailDelegate and AlertManagerDelegate methods in ProductDetail VC

--- QShopping/Views/ProductDetailViewController.swift
+++ QShopping/Views/ProductDetailViewController.swift
@@ -6,8 +6,12 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ProductDetailViewController: UIViewController {
+    
+    var productManager = ProductManager()
+    var alertManager = AlertManager()
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
@@ -21,8 +25,11 @@ class ProductDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        productManager.gettingProductDetailDelegate = self
+        alertManager.delegate = self
+        
+        productManager.getProduct(id: 1)
     }
 
     @IBAction func favoritePressed(_ sender: UIButton) {
@@ -32,5 +39,37 @@ class ProductDetailViewController: UIViewController {
     }
     
     @IBAction func addProductButtonPressed(_ sender: UIButton) {
+    }
+}
+//MARK: - GettingProductDetailDelegate
+
+extension ProductDetailViewController: GettingProductDetailDelegate {
+    func didSuccessGettingProductDetail(product: Product) {
+        DispatchQueue.main.async {
+            self.titleLabel.text = product.title
+            self.descriptionLabel.text = product.description
+            self.ratingLabel.text = String(product.rate)
+            self.reviewLabel.text = "\(product.reviews) reviews"
+            self.priceLabel.text = "$\(product.price)"
+            
+            let url = URL(string: product.imageURL)
+            self.imageView.kf.setImage(with: url)
+            self.addProductButton.subtitleLabel?.text = "$\(product.price)"
+        }
+    }
+    
+    func didFailGettingProductDetail(error: Error) {
+        DispatchQueue.main.async {
+            let alert = self.alertManager.errorAlert(for: error)
+            self.alertManager.show(alert: alert)
+        }
+    }
+    
+}
+//MARK: - AlertManagerDelegate
+
+extension ProductDetailViewController: AlertManagerDelegate {
+    func presentAlert(alertController: UIAlertController) {
+        self.present(alertController, animated: true)
     }
 }
