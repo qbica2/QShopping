b95fd13a938daad229e2ed80ccaa2e81cab136f9
Kubilay Akdemir <kubidik27@gmail.com>
2023-05-08 20:12:13 +0300

    create FilterCriteria

---
+++ QShopping/Models/FilterCriteria.swift
@@ -0,0 +1,13 @@
+//
+//  FilterCriteria.swift
+//  QShopping
+//
+//  Created by Mehmet Kubilay Akdemir on 8.05.2023.
+//
+
+import Foundation
+
+struct FilterCriteria {
+    var minRating: Double?
+    var minPrice: Double?
+}
