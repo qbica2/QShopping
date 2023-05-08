8264cd0b6ca48ada06f74e574565676d49bdab56
Kubilay Akdemir <kubidik27@gmail.com>
2023-05-08 17:28:59 +0300

    create SortingCriteria enum

---
+++ QShopping/Models/SortingCriteria.swift
@@ -0,0 +1,15 @@
+//
+//  SortingCriteria.swift
+//  QShopping
+//
+//  Created by Mehmet Kubilay Akdemir on 8.05.2023.
+//
+
+import Foundation
+
+enum SortingCriteria {
+    case priceAscending
+    case priceDescending
+    case rating
+    case reviews
+}
