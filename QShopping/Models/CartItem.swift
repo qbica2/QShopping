7fc1477422e4418ea07655cbb2f78f88f0b27873
Kubilay Akdemir <kubidik27@gmail.com>
2023-05-16 15:35:28 +0300

    create CartItem struct

---
+++ QShopping/Models/CartItem.swift
@@ -0,0 +1,13 @@
+//
+//  CartItem.swift
+//  QShopping
+//
+//  Created by Mehmet Kubilay Akdemir on 16.05.2023.
+//
+
+import Foundation
+
+struct CartItem {
+    let product: Product
+    let quantity: Int
+}
