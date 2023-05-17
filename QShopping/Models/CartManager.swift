f8fe96f9e5172be5c95e025a22293da736e371ee
Kubilay Akdemir <kubidik27@gmail.com>
2023-05-17 13:28:22 +0300

    implement addToCart method in CartManager

--- QShopping/Models/CartManager.swift
+++ QShopping/Models/CartManager.swift
@@ -12,14 +12,14 @@ class CartManager {
         
     var products: [CartItem] = []
     
-//    func addToCart(_ product: Product) {
-//        if let existingItemIndex = products.firstIndex(where: { $0.product.id == product.id }) {
-//            products[existingItemIndex].quantity += 1
-//        } else {
-//            let newItem = CartItem(product: product, quantity: 1)
-//            products.append(newItem)
-//        }
-////        NotificationCenter.default.post(name: NSNotification.Name("CartUpdated"), object: nil)
-//    }
+    func addToCart(_ product: Product) {
+        if let existingItemIndex = products.firstIndex(where: { $0.product.id == product.id }) {
+            products[existingItemIndex].quantity += 1
+        } else {
+            let newItem = CartItem(product: product, quantity: 1)
+            products.append(newItem)
+        }
+        NotificationCenter.default.post(name: NSNotification.Name("ProductAdded"), object: nil)
+    }
 }
 
