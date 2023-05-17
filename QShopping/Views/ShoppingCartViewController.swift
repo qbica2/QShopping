3dd1f0eecd39c331ae14d234827861b6086d0046
Kubilay Akdemir <kubidik27@gmail.com>
2023-05-17 12:59:09 +0300

    add UITableViewDataSource and UITableViewDelegate methods to ShoppingCart VC

--- QShopping/Views/ShoppingCartViewController.swift
+++ QShopping/Views/ShoppingCartViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ShoppingCartViewController: UIViewController {
 
@@ -18,7 +19,16 @@ class ShoppingCartViewController: UIViewController {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        
+//        NotificationCenter.default.addObserver(self, selector: #selector(cartUpdated), name: NSNotification.Name("CartUpdated"), object: nil)
+
     }
+    
+//    @objc func cartUpdated(){
+//        DispatchQueue.main.async {
+//            self.tableView.reloadData()
+//        }
+//    }
     
     
     @IBAction func emptyBasketButtonPressed(_ sender: UIBarButtonItem) {
@@ -29,20 +39,35 @@ class ShoppingCartViewController: UIViewController {
     
     
 }
+//MARK: - UITableViewDataSource
 
 extension ShoppingCartViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return CartManager.shared.products.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ShoppingCartCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cells.shoppingCartCell, for: indexPath) as! ShoppingCartCell
+        
+        let item = CartManager.shared.products[indexPath.row]
+        cell.priceLabel.text = "$ \(item.product.price)"
+        cell.titleLabel.text = item.product.title
+        cell.descriptionLabel.text = item.product.description
+        cell.quantityLabel.text = String(item.quantity)
+        
+        let url = URL(string: item.product.imageURL)
+        cell.itemImage.kf.setImage(with: url)
         
         return cell
     }
     
 }
+//MARK: - UITableViewDelegate
 
 extension ShoppingCartViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
     
 }
