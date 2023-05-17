02663b3d70358d40b8e5db5b938afc008566eb1a
Kubilay Akdemir <kubidik27@gmail.com>
2023-05-18 01:20:54 +0300

    add cartItemCount and item(at index:) methods to CartManager

--- QShopping/Models/CartManager.swift
+++ QShopping/Models/CartManager.swift
@@ -12,6 +12,29 @@ class CartManager {
         
     var products: [CartItem] = []
     
+    var cartItemCount: Int {
+        return products.count
+    }
+    
+    func item(at index: Int) -> CartItem? {
+        guard index >= 0 && index < products.count else {
+            return nil
+        }
+        return products[index]
+    }
+    
+//    func totalCartPrice() -> Double {
+//        var totalPrice: Double = 0.0
+//        
+//        for item in products {
+//            let productPrice = item.product.price
+//            let itemTotalPrice = productPrice * Double(item.quantity)
+//            totalPrice += itemTotalPrice
+//        }
+//        
+//        return totalPrice
+//    }
+    
     func addToCart(_ product: Product, quantity: Int = 1) {
         if let existingItemIndex = products.firstIndex(where: { $0.product.id == product.id }) {
             products[existingItemIndex].quantity += quantity
