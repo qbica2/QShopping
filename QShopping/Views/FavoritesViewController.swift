eb92be7500c0eaefed9b9ed6685378985e9dbd57
Kubilay Akdemir <kubidik27@gmail.com>
2023-05-26 13:27:21 +0300

    updated Favorites VC with UITableViewDataSource and UITableViewDelegate methods

--- QShopping/Views/FavoritesViewController.swift
+++ QShopping/Views/FavoritesViewController.swift
@@ -6,23 +6,70 @@
 //
 
 import UIKit
+import Kingfisher
 
 class FavoritesViewController: UIViewController {
 
-    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var primaryButton: UIButton!
     @IBOutlet weak var deleteButton: UIBarButtonItem!
     
+    var favoriteManager = FavoriteManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(update), name: NSNotification.Name(K.NotificationName.favUpdated), object: nil)
+    }
+    
+    @objc func update(){
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
     
     @IBAction func deleteButtonTapped(_ sender: UIBarButtonItem) {
     }
     
     @IBAction func primaryButtonTapped(_ sender: UIButton) {
+    }
+}
+//MARK: - UITableViewDataSource
+
+extension FavoritesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoriteManager.favoriteItemCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cells.favoriteCell, for: indexPath) as! FavoriteCell
+        
+        if let item = favoriteManager.item(at: indexPath.row) {
+            cell.priceLabel.text = "$ \(item.price)"
+            cell.descriptionLabel.text = item.description
+            cell.titleLabel.text = item.title
+            
+            let url = URL(string: item.imageURL)
+            cell.itemImage.kf.setImage(with: url)
+            
+            cell.selectedProduct = item
+            
+        }
+        
+        return cell
+    }
+    
+}
+//MARK: - UITableViewDelegate
+
+extension FavoritesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
